af491191a96bd98046d99a7eedca2c1fc2450bbe
yusef <youssefsamir432@gmail.com>
2025-01-26 09:24:30 +0200

    Remove add button for normal server members

--- frontend/src/components/ServerChannels.jsx
+++ frontend/src/components/ServerChannels.jsx
@@ -120,9 +120,12 @@ function ServerChannels({name, server,setCurrentChannel,createChannelDialog, set
 
         <button style={{all:'unset', marginLeft:'auto'}} onClick={(e) => {setCreateChannelDialog(!createChannelDialog)}}>
 
+        {isOwner ? 
+        
         <SVG3 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </SVG3>
+        : null}
         </button>
             </Bar>
 
